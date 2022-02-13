d761ab99610e95907741ebf845358d6374c51f03
kenchaso <kenchasonakai@gmail.com>
2022-02-13 12:33:52 +0900

    add: PostモデルでActiveStorageを使えるように設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(image: params[:post][:image], caption: params[:post][:caption])
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+
+  validates :image, presence: true
+  validates :caption, length: { maximum: 191 }
 end
