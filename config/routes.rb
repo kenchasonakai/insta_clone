789be197ef83eb9a313238692c19d15d2c9c8160
kenchaso <kenchasonakai@gmail.com>
2022-02-16 12:12:00 +0900

    add: ユーザー用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "posts#index"
   resources :posts
+  resources :users
+  get 'login', to: 'user_sessions#new', as: 'login' # ログイン画面用
+  post 'login', to: "user_sessions#create" # ログイン処理用
+  delete 'logout', to: 'user_sessions#destroy', as: 'logout' # ログアウト処理用
 end
