a0c659e7444051608cc66c545116c6d8e35cca66
kenchaso <kenchasonakai@gmail.com>
2022-03-06 18:31:40 +0900

    いいね用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "posts#index"
-  resources :posts
+  resources :posts do
+    resource :like, only: %i[create destroy]
+  end
   resources :users, param: :username
   get 'login', to: 'user_sessions#new', as: 'login'
   post 'login', to: "user_sessions#create"
