cd2a4332f071e5ebb12a937f3b7c2c96e8a7788e
kenchaso <kenchasonakai@gmail.com>
2022-03-21 17:15:07 +0900

    フォロー用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new', as: 'login'
   post 'login', to: "user_sessions#create"
   delete 'logout', to: 'user_sessions#destroy', as: 'logout'
+  resources :follows, only: [:create, :destroy]
 end
